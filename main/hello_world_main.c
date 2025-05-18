170c66acf4ae0b1c5206cae2bf2c8229c16de33b
LYNHQQ <844550332@qq.com>
2025-05-19 00:57:50 +0800

    update MahonyAHRS

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <inttypes.h>
+#include <math.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -14,6 +15,7 @@
 #include "esp_system.h"
 #include "esp_timer.h"
 #include "spi.h"
+#include "MahonyAHRS.h"
 
 #define IMU_CLK_IN_PIN  (gpio_num_t)(9)
 #define IMU_INT_PINNUM  (gpio_num_t)(14)
@@ -23,11 +25,11 @@
     #include "driver/ledc.h"
     #define CLK_IN_TIMER              LEDC_TIMER_0
     #define CLK_IN_MODE               LEDC_LOW_SPEED_MODE
-    #define CLK_IN_OUTPUT_IO          IMU_CLK_IN_PIN // Define the output GPIO
+    #define CLK_IN_OUTPUT_IO          IMU_CLK_IN_PIN
     #define CLK_IN_CHANNEL            LEDC_CHANNEL_0
-    #define CLK_IN_DUTY_RES           LEDC_TIMER_8_BIT // Set duty resolution to 13 bits
-    #define CLK_IN_DUTY               (128) // Set duty to 50%. (2 ^ 8) * 50% = 128
-    #define CLK_IN_FREQUENCY          (32000) // Frequency in Hertz. Set frequency at 32 kHz
+    #define CLK_IN_DUTY_RES           LEDC_TIMER_8_BIT
+    #define CLK_IN_DUTY               (128)
+    #define CLK_IN_FREQUENCY          (32000)
     static void clk_in_init(void);
 #endif
 
@@ -37,6 +39,7 @@
         volatile uint64_t start = 0;
         volatile uint64_t end = 0;
         volatile uint64_t time = 0;
+        volatile uint64_t last_timestamp = 0;
     #endif
 // 中断事件队列句柄
 static QueueHandle_t imu_queue = NULL;
@@ -48,15 +51,36 @@ float accel_mg[3] = {0};
 float gyro_dps[3] = {0};
 float temp_degc = 0;
 
+// 四元数到欧拉角 (Z-Y-X 顺规)
+// 输入: q0, q1, q2, q3 (四元数 w, x, y, z)
+// 输出: roll, pitch, yaw (弧度)
+void QuatToEuler(float q0, float q1, float q2, float q3, float* roll, float* pitch, float* yaw) {
+    // Roll (x-axis rotation)
+    float sinr_cosp = 2.0f * (q0 * q1 + q2 * q3);
+    float cosr_cosp = 1.0f - 2.0f * (q1 * q1 + q2 * q2);
+    *roll = atan2f(sinr_cosp, cosr_cosp);
+
+    // Pitch (y-axis rotation)
+    float sinp = 2.0f * (q0 * q2 - q3 * q1);
+    if (fabs(sinp) >= 1)
+        *pitch = copysignf(M_PI / 2.0f, sinp); // Use 90 degrees if out of range
+    else
+        *pitch = asinf(sinp);
+
+    // Yaw (z-axis rotation)
+    float siny_cosp = 2.0f * (q0 * q3 + q1 * q2);
+    float cosy_cosp = 1.0f - 2.0f * (q2 * q2 + q3 * q3);
+    *yaw = atan2f(siny_cosp, cosy_cosp);
+}
+
 void app_main(void)
 {
-
-    if(!setup_imu(1,1,1))
-        printf("IMU setup done\n");
-    else
-        printf("IMU setup failed\n");
 #ifdef IMU_INT_PINNUM
     imu_queue = xQueueCreate(32, sizeof(bool));
+    if (imu_queue == NULL) {
+        printf("Failed to create imu_queue\n");
+        return;
+    }
     // 创建数据处理任务
     xTaskCreatePinnedToCore(IMU_IRQ_process, "imu", 4096, NULL, 15, NULL, 1);
     // 配置中断输入引脚
@@ -68,24 +92,43 @@ void app_main(void)
     gpio_isr_handler_add(IMU_INT_PINNUM, IMU_IRQ_handler, NULL);
     // 配置为下降沿触发中断
     gpio_set_intr_type(IMU_INT_PINNUM, GPIO_INTR_POSEDGE);
+#endif
+    if(!setup_imu(1,1,1))
+        printf("IMU setup done\n");
+    else
+        printf("IMU setup failed\n");
     // 使能中断
     gpio_intr_enable(IMU_INT_PINNUM);
-#endif
+
 #ifdef IMU_CLK_IN_PIN
     clk_in_init();
     ledc_set_duty(CLK_IN_MODE, CLK_IN_CHANNEL, CLK_IN_DUTY);
     ledc_update_duty(CLK_IN_MODE, CLK_IN_CHANNEL);
 #endif
+
     while(1)
     {
         vTaskDelay(pdMS_TO_TICKS(1000));
         // bsp_IcmGetRawData(accel_mg, gyro_dps,&temp_degc);
-        printf("accel_mg: %f %f %f\n", accel_mg[0], accel_mg[1], accel_mg[2]);
-        printf("gyro_dps: %f %f %f\n", gyro_dps[0], gyro_dps[1], gyro_dps[2]);
-        printf("temp_degc: %f\n", temp_degc);
+        // printf("accel_mg: %f %f %f\n", accel_mg[0], accel_mg[1], accel_mg[2]);
+        // printf("gyro_dps: %f %f %f\n", gyro_dps[0], gyro_dps[1], gyro_dps[2]);
+        // printf("temp_degc: %f\n", temp_degc);
+
+        // 获取 MahonyAHRS 计算出的四元数
+        float current_q0 = q0;
+        float current_q1 = q1;
+        float current_q2 = q2;
+        float current_q3 = q3;
+
+        // 将四元数转换为欧拉角
+        float roll, pitch, yaw;
+        QuatToEuler(current_q0, current_q1, current_q2, current_q3, &roll, &pitch, &yaw);
+
+        // 将弧度转换为度并打印
+        printf("Roll=%.2f, Pitch=%.2f, Yaw=%.2f\n", roll * 180.0f/M_PI, pitch * 180.0f/M_PI, yaw * 180.0f/M_PI);
 
 #ifdef IMU_TICK
-        printf("Time: %llu us\n", time);
+        // printf("Time: %llu us\n", time);
 #endif
     }
 }
@@ -104,27 +147,50 @@ static void IMU_IRQ_handler(void* arg)
 void IMU_IRQ_process(void *pvParameters)
 {
     bool res;
+    uint64_t current_timestamp;
+    float dt; // 时间间隔，单位秒
+
+    last_timestamp = esp_timer_get_time();
+
     while (1)
     {
-        // 等待中断事件
         xQueueReceive(imu_queue, &res, portMAX_DELAY);
         if (res)
         {
-            #ifdef IMU_TICK
-            if(flag0)
-            {
-                start = esp_timer_get_time();
-                flag0 = 0;
-            }
-            else
-            {
-                end = esp_timer_get_time();
-                time = end - start;
-                flag0 = 1;
-            } 
-            #endif
-            /*获取ICM45686*/
             bsp_IcmGetRawData(accel_mg, gyro_dps,&temp_degc);
+
+            current_timestamp = esp_timer_get_time();
+            dt = (float)(current_timestamp - last_timestamp) / 1000000.0f;
+            last_timestamp = current_timestamp;
+
+            if (dt > 0 && dt < 0.1f) {
+                 // 将 dps 转换为 rad/s
+                 float gx_rads = gyro_dps[0] * M_PI / 180.0f;
+                 float gy_rads = gyro_dps[1] * M_PI / 180.0f;
+                 float gz_rads = gyro_dps[2] * M_PI / 180.0f;
+
+                 // 将 mg 转换为 g
+                 float ax_g = accel_mg[0] / 1000.0f;
+                 float ay_g = accel_mg[1] / 1000.0f;
+                 float az_g = accel_mg[2] / 1000.0f;
+
+                 // 调用 MahonyAHRS 更新函数 (根据头文件，不传入 dt)
+                 MahonyAHRSupdateIMU(gx_rads, gy_rads, gz_rads, ax_g, ay_g, az_g);
+            }
+
+            #ifdef IMU_TICK
+            // if(flag0)
+            // {
+            //     start = esp_timer_get_time();
+            //     flag0 = 0;
+            // }
+            // else
+            // {
+            //     end = esp_timer_get_time();
+            //     time = end - start;
+            //     flag0 = 1;
+            // }
+            #endif
         }
     }
 }
